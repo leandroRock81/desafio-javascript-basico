0589897ea9e8ef0a7a5ef04b281b55da6f9b2d86
Leandro Adriano <lerockadriano@gmail.com>
2022-06-20 19:41:20 -0300

    desafio javascript basico

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function somar(numero1, numero2){
+    let soma = numero1 + numero2
+   console.log('A soma de ' + numero1 + ' e ' + numero2 + ' é ' + soma)
+}
+
+function dividir(numero1, numero2){
+    let divisao = numero1 / numero2
+    console.log('A divisão de ' + numero1 + ' e ' + numero2 + ' é ' + divisao)
+}
+
+function multiplicar(numero1, numero2){
+    let multiplicacao = numero1 * numero2
+    console.log('A multiplicação de ' + numero1 + ' e ' + numero2 + ' é ' + multiplicacao)
+}
+
+function subtrair(numero1, numero2){
+    let subtracao = numero1 - numero2
+    console.log('A subtração de ' + numero1 + ' e ' + numero2 + ' é ' + subtracao)
+}
+
+
+
+somar(3, 2)
+dividir(3, 2)
+multiplicar(3, 2)
+subtrair(3, 2)
+
+
+
+
+
+
